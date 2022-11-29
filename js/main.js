a1c69d8f40f48a3c63f90a761f20d230dc32981f
AgusHeredia <agusherediatbb@gmail.com>
2022-11-29 19:20:38 -0300

    funcion de saludo y validacion de datos mejorada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-//Mensaje de Bienvenida
-let userName = prompt("Por favor, ingrese su nombre")
-let userLastName = prompt("Por favor, ingrese su apellido")
+// ========= MENSAJE DE BIENVENIDA Y VALIDACIÓN DE DATOS PERSONALES ========= //
 
-function sayHello(name, lastName) {
-    alert("Hola " + name + " " + lastName + "! Bienvenido a mi página web :)")
+//Pedimos al usuario que ingrese sus datos
+let userName = ""
+let userLastName = ""
+let datosCorrectos = true
+
+//Declaramos la funcion para ejecutar el saludo
+const pedirDatos = () => {
+
+    do {
+        userName = prompt("Por favor, ingrese su nombre:")
+        userLastName = prompt("Por favor, ingrese su apellido:")
+
+        if (userName != "" && userLastName != "") {
+            alert("¡Hola " + userName + " " + userLastName + "! \nBienvenido/a a mi tienda online de productos Apple, donde vas a encontrar los mejores precios del mercado con hasta 12 meses de garantía. Y para tu tranquilidad, contamos con dispositivos 100% ORIGINALES, exportados directamente desde USA hasta la puerta de tu casa :)")
+            datosCorrectos = false
+
+        } else {
+            alert("Error: revisa tus datos y completa los campos solicitados correctamente")
+        }
+
+    } while (datosCorrectos);
 }
 
-sayHello(userName, userLastName)
+//Llamamos a la función
+pedirDatos(userName, userLastName)
+
+
+
+
